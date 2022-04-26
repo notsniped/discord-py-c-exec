df3718e1a3381ac8eb88999025e37c8849aae939
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2022-04-26 19:56:26 +0300

    Update file.py

--- file.py
+++ file.py
@@ -1,10 +1,9 @@
 import os, sys, discord, subprocess
 from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
-from discord_slash.utils.manage_commands import create_option
+from dislash import InteractionClient, Option, OptionType
 
 client = commands.Bot(command_prefix=".")
-slash = SlashCommand(client, sync_commands=True)
+slash = InteractionClient(client)
 
 @client.event
 async def on_ready(): print("o/")
@@ -26,22 +25,21 @@ async def cexec(ctx, *, body):
 	p = subprocess.check_output("./a.out", shell=True)
 	return await ctx.send(f"```c\n{p.decode()}\n```")
 
-@slash.slash(
-    name='cexec',
-    description='Lets you execute c code',
-    options=[
-        create_option(name='body', option_type=3, required=True
-    ]
-)
-async def slash_cexec(ctx, *, body):
+@slash.slash_command(
+    name="cexec",
+    description="executes c code",
+    options=[Option("body", "type your code", OptionType.STRING, required=True)]
+    )
+async def _cexec(slash, *, body):
+	body = cleanup_code(body)
 	if os.path.isfile("./a.out"): os.system("rm -r ./a.out")
 	if os.path.isfile("./file1.c"): os.system("rm -r ./file1.c")
 	f = open("./file1.c", "w")
 	f.write(body)
 	f.close()
 	c = subprocess.run("gcc ./file1.c", capture_output=True, text=True, shell=True)
-	if c.returncode != 0: return await ctx.send(f"```c\n{c.stderr}```")
+	if c.returncode != 0: return await slash.reply(f"```c\n{c.stderr}```")
 	p = subprocess.check_output("./a.out", shell=True)
-	return await ctx.send(f"```c\n{p.decode()}\n```")	    
+	return await slash.reply(f"```c\n{p.decode()}\n```")	    
 
 client.run(token)
